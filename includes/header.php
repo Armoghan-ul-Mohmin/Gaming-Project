fc5c7cec24052a8852f7e2f011688ef40097a9d7
Armoghan <Armoghan16@gmail.com>
2023-07-15 23:37:05 +0500

    Added Header and Scripts

--- includes/header.php
+++ includes/header.php
@@ -2,9 +2,30 @@
 <html>
 
 <head>
-    <title>Online Gaming Platform</title>
-    <link rel="stylesheet" type="text/css" href="../css/main.css">
-    <script src="../js/main.js"></script>
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<body>
+    <!-- Bootstrap Css -->
+    <link rel="stylesheet" href="static/bootstrap/css/bootstrap.css">
+
+    <!-- Theme Css -->
+    <link rel="stylesheet" href="static/theme/theme.css">
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="static/css/main.css">
+
+    <!-- Font Awsome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+    <!-- Scroll Bar Css -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+    <!-- Google Fonts  -->
+    <link rel=" preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- font-family: 'Caprasimo', cursive; -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caprasimo&family=Poppins:wght@400;500;600;700&display=swap"
+        rel="stylesheet"> <!-- font-family: 'Caprasimo', cursive;    font-family: 'Poppins', sans-serif; -->
---
+++ includes/scripts.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap Js -->
+<script src="static/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="static/bootstrap/js/bootstrap.esm.js"></script>
+<script src="static/bootstrap/js/bootstrap.js"></script>
+
+<!-- JQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Custom Js -->
+<script src="static/js/main.js"></script>
